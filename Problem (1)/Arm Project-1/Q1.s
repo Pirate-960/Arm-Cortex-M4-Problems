bc6992ea2ea1e11a3e08f36d15bf96d31b6c61c5
Pirate-960 <101503244+Pirate-960@users.noreply.github.com>
2024-11-01 16:08:09 +0300

    Fix

--- Problem (1)/Arm Project-1/Q1.s
+++ Problem (1)/Arm Project-1/Q1.s
@@ -2,27 +2,42 @@
     INCLUDE stm32l476xx_constants.s      
 		
 
-	AREA P1, CODE 
-	EXPORT __main 
-	ALIGN 
-	ENTRY 
+	AREA P1, CODE
+	EXPORT __main
+	ALIGN
+	ENTRY
 
-__main PROC 
+__main PROC
+; Setting Up Registers (Local Variables)
+; r0 = total
+; r1 = num1 (first input)
+; r2 = num2 (second input - number of terms)
+; r3 = multiplyFactor (for digit count)
+; r4 = sum (new term)
+; r5 = loop counter
+; r6 = temp (for digit counting)
+	
+	; Initialize variables
+	MOV r0, #0	; Total = 0
+	MOV r1, #45	; First Number = 45
+	MOV r2, #3	; Second Number = 3
+	MOV r3, #0	; Loop Counter - i = 0
+	MOV r4, #0	; Sum -> Terms Accmulator = 0
+	MOV r5, #1	; multiplyFactor = 1
+	MOV r7, #10 ; constant value 10 for multiplication and division
+	
+	MOV r6, r1	; Temp = Num 1
+	
+	; multiplyFactor Calculation using No. Of Digits in (num1)
+L1
+    CMP r6, r7         ; Check if r6 < 10
+    BLT digitCountDone ; If r6 < 10, we are done
+    MUL r3, r3, r7     ; multiplyFactor *= 10
+    SDIV r6, r6, r7    ; Divide temp by 10
+    B L1
 
-	MOV r0, #0 ; total
-	;r1 = input = a 
-	;r2 = input = b 
-	MOV r1, #45 
-	MOV r2, #3
-	
-	; r3 = loop counter
-	MOV r3, #0
-	; r4 = new sum
-	MOV r4, #0
-	; r5 = 10 imm value for multiplication
-	MOV r5, #10
-	
-loop 
+L2
+L3 
 
 	; sum = sum * 10 + a
 	; total = total + sum
@@ -33,7 +48,7 @@ loop
 	ADD r0, r0, r4
 	
 	ADD r3, r3, #1
-	B loop
+	B L3
 	
 	
 stop B stop
